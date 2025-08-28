ac366e0d8f71a3e36a574bec1ac1821745550b20
Peter Neutron <introns-bungles0p@icloud.com>
2025-08-28 15:13:43 +0200

    Initial commit

---
+++ Illumination/Illumination/IlluminationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IlluminationApp.swift
+//  Illumination
+//
+//
+//
+
+import SwiftUI
+
+@main
+struct IlluminationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
